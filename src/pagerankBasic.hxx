e27c9097d15259d4389429f8f6d665a56f7a55f7
Subhajit Sahu <wolfram77@gmail.com>
2023-07-15 16:37:38 +0530

    :bug: use frontier tolerance = tolerance/1000

--- src/pagerankBasic.hxx
+++ src/pagerankBasic.hxx
@@ -173,7 +173,7 @@ inline PagerankResult<V> pagerankBasicDynamicTraversalOmp(const G& x, const H& x
  */
 template <bool ASYNC=false, bool DEAD=false, class FLAG=char, class G, class H, class K, class V, class FV>
 inline PagerankResult<V> pagerankBasicDynamicFrontierSeq(const G& x, const H& xt, const G& y, const H& yt, const vector<tuple<K, K>>& deletions, const vector<tuple<K, K>>& insertions, const vector<V> *q, const PagerankOptions<V>& o, FV fv) {
-  V D = 0.01 * o.tolerance;  // see adjust-tolerance
+  V D = 0.001 * o.tolerance;  // see adjust-tolerance
   if (xt.empty()) return {};
   vector<FLAG> vaff(max(x.span(), y.span()));
   return pagerankSeq<ASYNC>(yt, q, o, [&](auto& e, vector<V>& a, vector<V>& r, const H& xt, V P, V E, int L, int EF, vector<ThreadInfo*>& threads) {
@@ -188,7 +188,7 @@ inline PagerankResult<V> pagerankBasicDynamicFrontierSeq(const G& x, const H& xt
 #ifdef OPENMP
 template <bool ASYNC=false, bool DEAD=false, class FLAG=char, class G, class H, class K, class V, class FV>
 inline PagerankResult<V> pagerankBasicDynamicFrontierOmp(const G& x, const H& xt, const G& y, const H& yt, const vector<tuple<K, K>>& deletions, const vector<tuple<K, K>>& insertions, const vector<V> *q, const PagerankOptions<V>& o, FV fv) {
-  V D = 0.01 * o.tolerance;  // see adjust-tolerance
+  V D = 0.001 * o.tolerance;  // see adjust-tolerance
   if (xt.empty()) return {};
   vector<FLAG> vaff(max(x.span(), y.span()));
   return pagerankOmp<ASYNC>(yt, q, o, [&](auto& e, vector<V>& a, vector<V>& r, const H& xt, V P, V E, int L, int EF, vector<ThreadInfo*>& threads) {
